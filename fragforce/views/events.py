438f21b92f8f512f36a41e6f7883de5ce8899dc2
AevumDecessus <aevum@decess.us>
2016-03-28 18:17:31 -0400

    Fixed blueprint name for events

--- fragforce/views/events.py
+++ fragforce/views/events.py
@@ -2,7 +2,7 @@ from fragforce import app
 from flask import Blueprint, render_template, session, redirect, url_for, \
         request, abort
 
-mod = Blueprint('general', __name__, url_prefix='/events')
+mod = Blueprint('events', __name__, url_prefix='/events')
 
 @mod.route('/')
 def index(name=None):
