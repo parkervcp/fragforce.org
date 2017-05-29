47adba5afdf94cf5c06706073b443c4155b91a8b
Paulson McIntyre <paul+git@gpmidi.net>
2017-05-29 17:28:56 -0400

    Removing extra check - the regex should keep it safe

--- fragforce/views/general.py
+++ fragforce/views/general.py
@@ -43,9 +43,9 @@ def pygments_css():
 def firewall_tables(table_type, fname):
     if table_type not in ['ports', 'urls', 'nets']:
         abort(404)
-    if fname not in os.listdir(os.path.join(app.instance_path, app.template_folder, 'fwaliases', table_type)):
-        abort(404)
+    # if fname not in os.listdir(os.path.join(app.instance_path, app.template_folder, 'fwaliases', table_type)):
+    #     abort(404)
     if not RE_FW_TABLE_NAME.match(fname):
         abort(404)
 
-    return render_template('fwaliases/%s/%s' % (table_type, fname), table_type=table_type)
+    return render_template('fwaliases/%s/%s' % (table_type, fname), table_type=table_type, base_name=fname)
