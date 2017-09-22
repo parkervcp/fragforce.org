06b4a6b1b8aa51023f355011927f87dbe757e0bc
Paulson McIntyre <paul+public@gpmidi.net>
2017-09-22 11:55:44 -0400

    Adding view to generate a pfsense compatible backup of all aliases

--- fragforce/views/general.py
+++ fragforce/views/general.py
@@ -7,6 +7,7 @@ import os
 
 mod = Blueprint('general', __name__)
 RE_FW_TABLE_NAME = re.compile(r'^[a-zA-Z0-9]+\.(nets|ports|urls|ips)$')
+FW_ALIAS_PATH_FIXER = re.compile(r'^(https://.*/)firewalls/aliases.*')
 
 
 @mod.route('/')
@@ -62,3 +63,44 @@ def firewall_tables_ports(table_type, fname):
         abort(404)
 
     return render_template('fwaliases/%s/%s' % (table_type, fname), table_type=table_type, base_name=fname)
+
+
+@mod.route('/firewalls/aliases/backup.xml')
+def alias_backup_gen():
+    """ Return an XML doc that can be restored via the pfsense backup interface. It will load all aliases. """
+    from fragforce.pfsense import AliasBackup
+    import os, os.path
+    import urllib
+
+    root_url = FW_ALIAS_PATH_FIXER.match(request.base_url)[0]
+
+    port_path = os.path.join(app.config['BASE_DIR'], 'templates', 'fwaliases', 'ports')
+    nets_path = os.path.join(app.config['BASE_DIR'], 'templates', 'fwaliases', 'nets')
+
+    ab = AliasBackup()
+
+    def visit_port(ab, dirname, names):
+        for file_name in names:
+            path = os.path.join(dirname, file_name)
+            if file_name.endswith('.ports'):
+                url = root_url + "/firewalls/tables/ports/" + file_name
+                ab.add_port_alias(name=file_name.rstrip('.ports'), url=url, update_frequency_days=1,
+                                  description="Port Table %r" % file_name)
+
+    def visit_nets(ab, dirname, names):
+        for file_name in names:
+            path = os.path.join(dirname, file_name)
+            folder = os.path.split(dirname)[-1]
+            if file_name.endswith('.nets'):
+                url = root_url + "/firewalls/tables/nets/%s/%s"%(folder,file_name)
+                ab.add_ip_alias(name=file_name.rstrip('.nets'), url=url, update_frequency_days=1,
+                                description='Network Table %r' % file_name)
+            elif file_name.endswith('.ips'):
+                url = root_url + "/firewalls/tables/nets/%s/%s"%(folder,file_name)
+                ab.add_ip_alias(name=file_name.rstrip('.ips'), url=url, update_frequency_days=1,
+                                description='IP Table %r' % file_name)
+
+    os.path.walk(port_path, visit_port, ab)
+    os.path.walk(port_path, visit_nets, ab)
+
+    return ab.render(pretty=True)
