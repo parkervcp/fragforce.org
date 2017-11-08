f7c021d86a036b6cac5e296abfb0dd2289a050ea
Paulson McIntyre <pmcintyre@salesforce.com>
2017-11-08 09:23:53 -0500

    More info

--- fragforce/models.py
+++ fragforce/models.py
@@ -1,3 +1,11 @@
-from fragforce import db
-from sqlalchemy.dialects.postgresql import JSON
+""" Database schema
+
+WARNING: DON'T FORGET TO RUN 'alembic revision --autogenerate -m "<message>"' AFTER ANY CHANGES TO THE SCHEMA!!!!
+"""
+import datetime
+from sqlalchemy import *
+from sqlalchemy.orm import relation, backref, deferred
+from sqlalchemy.dialects.postgresql import UUID, ENUM, HSTORE, ARRAY, JSONB
+from fragforce import app, Base, RemoteBaseMeta, engine
+import uuid
 
