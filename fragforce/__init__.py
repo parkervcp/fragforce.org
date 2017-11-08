b6f565bec78394366c9eaa71f186ace315a193ed
Paulson McIntyre <pmcintyre@salesforce.com>
2017-11-08 09:14:30 -0500

    DATABASE_URI -> DATABASE_URL

--- fragforce/__init__.py
+++ fragforce/__init__.py
@@ -23,8 +23,8 @@ app.config['DEBUG'] = bool(os.environ.get('DEBUG', 'False').lower() == 'true')
 app.config['BASE_DIR'] = os.path.abspath(os.path.dirname(__file__))
 app.config['THREADS_PER_PAGE'] = 2
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'insecure')
-app.config['DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres://postgres@localhost:5432/fragforce')
-app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE_URI']
+app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL', 'postgres://postgres@localhost:5432/fragforce')
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE_URL']
 app.config['DATABASE_CONNECT_OPTIONS'] = {}
 app.config['REDIS_URL'] = os.environ.get('REDIS_URL', None)
 app.config['EXTRALIFE_TEAMID'] = os.environ.get('EXTRALIFE_TEAMID', None)
