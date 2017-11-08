78efebc628f4da43d6c3626a81390f72b226b0a5
Paulson McIntyre <pmcintyre@salesforce.com>
2017-11-08 09:27:59 -0500

    Bogus revision

---
+++ alembic/versions/6e5ef525f3cf_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6e5ef525f3cf
+Revises: fd3ef845e755
+Create Date: 2017-11-08 09:27:39.563417
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6e5ef525f3cf'
+down_revision = 'fd3ef845e755'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
