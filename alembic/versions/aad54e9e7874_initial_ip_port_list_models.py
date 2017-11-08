93673b9e390fed59b5dc4f1f2158e0cc46f2986b
Paulson McIntyre <pmcintyre@salesforce.com>
2017-11-08 10:34:54 -0500

    Fixing a new model issues and adding db revision

---
+++ alembic/versions/aad54e9e7874_initial_ip_port_list_models.py
@@ -0,0 +1,24 @@
+"""Initial ip/port list models
+
+Revision ID: aad54e9e7874
+Revises: 9f47e7ae5517
+Create Date: 2017-11-08 10:32:08.155983
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'aad54e9e7874'
+down_revision = '9f47e7ae5517'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- fragforce/models.py
+++ fragforce/models.py
@@ -30,7 +30,7 @@ class Contacts(Base):
     id = Column(Integer, primary_key=True)
     guid = Column(UUID, unique=True, default=lambda: str(uuid.uuid4()), index=True)
     display_name = Column(String(255), unique=True, nullable=False)
-    location_id = Column(Integer, ForeignKey('location.id'))
+    location_id = Column(Integer, ForeignKey('locations.id'))
     location = relation(Location, backref=backref('contacts', order_by=display_name))
 
 
