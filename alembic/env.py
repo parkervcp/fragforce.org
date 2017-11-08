abe2980bc1b703d30513fa58943f92d2d2e57b2a
Paulson McIntyre <pmcintyre@salesforce.com>
2017-11-08 09:25:38 -0500

    Fixing env import

--- alembic/env.py
+++ alembic/env.py
@@ -16,8 +16,8 @@ fileConfig(config.config_file_name)
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 #target_metadata = None
-from fragforce import db
-target_metadata = db.Model
+from fragforce import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
