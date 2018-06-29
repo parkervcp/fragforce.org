c3f875396fd86a77d9317f88fdfa58a6ac7e4a2a
Paulson McIntyre <paul@gpmidi.net>
2018-06-28 22:22:13 -0400

    Testing reverse

--- fragforce/views/pages.py
+++ fragforce/views/pages.py
@@ -157,7 +157,7 @@ def by_site(sfid):
     evts = db_session.query(ff_events).filter_by(site__c=act.sfid).filter(
         ff_events.columns.event_end_date__c >= datetime.utcnow()).order_by('event_start_date__c').all()
     old_evts = db_session.query(ff_events).filter_by(site__c=act.sfid).filter(
-        ff_events.columns.event_end_date__c < datetime.utcnow()).order_by('-event_start_date__c').all()
+        ff_events.columns.event_end_date__c < datetime.utcnow()).order_by('event_start_date__c').reverse().all()
     return render_template(templates, section='events', events=evts, old=old_evts, account=act)
 
 
