7af701d02b25b1387469cb2f4f26bec9b182cc4d
Paulson McIntyre <pmcintyre@salesforce.com>
2018-06-29 12:22:02 -0400

    Fixing debug get fallback type

--- fforg/settings.py
+++ fforg/settings.py
@@ -21,7 +21,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG', True).lower() == 'true')
+DEBUG = bool(os.environ.get('DEBUG', 'True').lower() == 'true')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
