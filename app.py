3ea0cd9343051051c19d57fe7d891fb2d54969cb
Paulson McIntyre <pmcintyre@salesforce.com>
2018-06-29 07:05:00 -0400

    Removing old stuff

--- app.py
+++
@@ -1,8 +0,0 @@
-import os
-from fragforce import app
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    dbg = bool(os.environ.get('DEBUG', 'False'))
-    listen = os.environ.get('LISTEN', '0.0.0.0')
-    app.run(host=listen, port=port, debug=dbg)
