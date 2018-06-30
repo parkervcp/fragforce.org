c807b44b2aed1a3202879a91f70ee161065dbcf7
Paulson McIntyre <paul@gpmidi.net>
2018-06-29 23:26:33 -0400

    Testing new js based local

--- fragforce/static/js/common.js
+++ fragforce/static/js/common.js
@@ -4,3 +4,9 @@ function toLocal(dt) {
     dt=new Date(dt);
     return dt.toString();
 }
+
+// Update an object's value to tz
+function updateTimeValue(id,dt) {
+    $(id).text(toLocal(dt));
+    $(id).setAttribute("datetime",dt);
+}
