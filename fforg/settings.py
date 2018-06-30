015b4d6e618556eddaf404f5ffb633cc647b137a
Paulson McIntyre <paul@gpmidi.net>
2018-06-30 00:30:26 -0400

    Adding ffsite and restricting admin site to debug mode

--- fforg/settings.py
+++ fforg/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'ffsite',
 ]
 
 MIDDLEWARE = [
--- ffsite/urls.py
+++ ffsite/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
+from django.urls import path
 
 urlpatterns = [
-    path('', include('ffsite.urls')),
+    path('admin/', admin.site.urls),
 ]
-
-# Only enable the admin site when in debug mode (for now)
-if settings.DEBUG:
-    urlpatterns.append(path('admin/', admin.site.urls))
