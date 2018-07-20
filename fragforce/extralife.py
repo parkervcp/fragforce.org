95e315a27c8d63cb9c9198ffb79d004654a58876
Paulson McIntyre <pmcintyre@salesforce.com>
2018-07-20 07:57:55 -0400

    Better log messages

--- fragforce/extralife.py
+++ fragforce/extralife.py
@@ -31,11 +31,14 @@ def fetch_json(url, **kwargs):
                   extra=extra)
 
     if current is not None and current > 0:
-        log.debug("Not going to fetch %r%r from %r using key %r: Current[%r] > 0", url, kwargs, host, fail_key, current,
-                  extra=extra)
         # sleep(current)
         if random.randint(0,current)!=0:
+            log.debug("Not going to fetch %r%r from %r using key %r: Current[%r] > 0", url, kwargs, host, fail_key, current,
+                      extra=extra)
             return None
+        else:
+            log.debug("Going to retry: Fetching %r%r from %r using key %r: Current[%r] > 0", url, kwargs, host, fail_key, current,
+                      extra=extra)
 
     def final(ok=True):
         extra = extra.copy()
