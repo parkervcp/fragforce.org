33c6d2d8cf9c347b74987e61dfaa9ddcf25e5785
Paulson McIntyre <pmcintyre@salesforce.com>
2018-10-05 13:41:55 -0400

    Adding more tiltify skel

--- ffdonations/tasks/tiltify/helpers.py
+++ ffdonations/tasks/tiltify/helpers.py
@@ -3,8 +3,8 @@ from django.conf import settings
 
 def get_tiltify(*args, **kwargs):
     """ Easy way to get a Tiltify obj """
-    from tiltify2.tiltify import Tiltify
-    return Tiltify(
+    from tiltify2.tiltify import Tiltify2
+    return Tiltify2(
         api_key=settings.TILTIFY_TOKEN,
         timeout=settings.TILTIFY_TIMEOUT,
         **kwargs
