d42ad69953dea86d8a1ec17ec7b92021a4031fc3
Paulson McIntyre <paul@gpmidi.net>
2018-10-05 00:53:48 -0400

    Normamlizing URLs in extralifeapi

--- extralifeapi/teams.py
+++ extralifeapi/teams.py
@@ -24,11 +24,17 @@ mod_logger = root_logger.getChild('teams')
 
 
 class Teams(DonorDriveBase):
-    URL_TEAMS = 'teams'
-    URL_EVENTS = 'events'
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def sub_team(self):
+        return 'teams'
+
+    def sub_team_by_tid(self, teamID):
+        return 'teams/%d' % teamID
+
+    def sub_team_by_eid(self, eventID):
+        return 'events/%d/teams' % eventID
 
     @classmethod
     def _team_to_team(cls, data):
@@ -42,13 +48,13 @@ class Teams(DonorDriveBase):
 
     def teams(self):
         """ Return a generator of all teams as Team named tuples """
-        fresp = self.fetch(sub_url=self.URL_TEAMS)
+        fresp = self.fetch(sub_url=self.sub_team)
         for t in fresp:
             yield self._team_to_team(t)
 
     def team(self, teamID):
         """ Get a team """
-        fresp = self.fetch(sub_url=urljoin(self.URL_TEAMS, str(teamID)))
+        fresp = self.fetch(sub_url=self.sub_team_by_tid(teamID))
         return self._team_to_team(fresp)
 
     def event_teams(self, eventID):
