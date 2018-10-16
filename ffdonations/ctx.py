c0900168ba6b402cde83ca5fdb7d3262200259da
Paulson McIntyre <pmcintyre@salesforce.com>
2018-10-15 20:15:24 -0400

    Adding django-memoize for caching some helper functions

--- ffdonations/ctx.py
+++ ffdonations/ctx.py
@@ -1,10 +1,11 @@
 from .models import *
 import datetime
 from django.conf import settings
+from .utils import *
 
 
 def donations(request):
     """ Context processors for all ffdonations pages """
     return dict(
-
+        el_donation_stats=el_donation_stats(),
     )
---
+++ ffdonations/utils.py
@@ -0,0 +1,22 @@
+from ..tasks import *
+from django.db.models import Q, Avg, Max, Min, Sum
+from django.conf import settings
+from .models import *
+from memoize import memoize
+
+
+@memoize(timeout=120)
+def el_num_donations():
+    baseq = DonationModel.objects.filter(DonationModel.tracked_q())
+    return baseq.count()
+
+
+@memoize(timeout=120)
+def el_donation_stats():
+    baseq = DonationModel.objects.filter(DonationModel.tracked_q())
+    return baseq.aggregate(
+        sumDonations=Sum('amount'),
+        avgDonation=Avg('amount'),
+        minDonation=Min('amount'),
+        maxDonation=Max('amount'),
+    )
