e2cb1a76be72484dc3c80aa626a03d0bf4e6278c
Paulson McIntyre <pmcintyre@salesforce.com>
2019-02-01 10:11:23 -0500

    Fixed to use EL IDs from org

--- ffdonations/utils.py
+++ ffdonations/utils.py
@@ -3,6 +3,8 @@ from django.db.models import Q, Avg, Max, Min, Sum
 # from django.conf import settings
 from .models import *
 from memoize import memoize
+from ffsfdc.models import *
+from django.utils import timezone
 
 
 # @memoize(timeout=120)
@@ -22,9 +24,48 @@ from memoize import memoize
 #     )
 
 @memoize(timeout=120)
-def el_num_donations():
-    tsum = TeamModel.objects.filter(tracked=True).aggregate(ttl=Sum('numDonations')).get('ttl', 0)
-    psum = ParticipantModel.objects.filter(Q(Q(team__tracked=False) | Q(team__isnull=True)), tracked=True) \
+def event_name_maker(year=timezone.now().year):
+    return 'Extra Life %d' % year
+
+
+@memoize(timeout=120)
+def el_teams(year=timezone.now().year):
+    """ Returns a list of team IDs that we're tracking for the given year """
+    yr = event_name_maker(year=year)
+    ret = []
+    for sa in SiteAccount.objects.filter(el_id__isnull=False).only('el_id').all():
+        try:
+            tm = TeamModel.objects.get(id=sa.el_id)
+            if tm.event.name == yr:
+                ret.append(tm.id)
+        except TeamModel.DoesNotExist:
+            pass
+    return ret
+
+
+@memoize(timeout=120)
+def el_contact(year=timezone.now().year):
+    """ Returns a list of participant IDs that we're tracking for the given year """
+    yr = event_name_maker(year=year)
+    ret = []
+    for sa in Contact.objects.filter(extra_life_id__isnull=False).only('extra_life_id').all():
+        try:
+            tm = ParticipantModel.objects.get(id=sa.extra_life_id)
+            if tm.event.name == yr:
+                ret.append(tm.id)
+        except ParticipantModel.DoesNotExist:
+            pass
+    return ret
+
+
+@memoize(timeout=120)
+def el_num_donations(year=timezone.now().year):
+    """ For current year """
+    tsum = TeamModel.objects.filter(id__in=el_teams(year=year)) \
+        .aggregate(ttl=Sum('numDonations')).get('ttl', 0)
+    psum = ParticipantModel.objects.filter(Q(
+        Q(team__tracked=False) | Q(id__in=el_teams(year=year))
+    ), tracked=True) \
         .aggregate(ttl=Sum('numDonations')).get('ttl', 0)
     return dict(
         countDonations=float(tsum + psum),
@@ -34,9 +75,13 @@ def el_num_donations():
 
 
 @memoize(timeout=120)
-def el_donation_stats():
-    tsum = TeamModel.objects.filter(tracked=True).aggregate(ttl=Sum('sumDonations')).get('ttl', 0)
-    psum = ParticipantModel.objects.filter(Q(Q(team__tracked=False) | Q(team__isnull=True)), tracked=True) \
+def el_donation_stats(year=timezone.now().year):
+    """ For current year """
+    tsum = TeamModel.objects.filter(id__in=el_teams(year=year)) \
+        .aggregate(ttl=Sum('sumDonations')).get('ttl', 0)
+    psum = ParticipantModel.objects.filter(Q(
+        Q(team__tracked=False) | Q(id__in=el_teams(year=year))
+    ), tracked=True) \
         .aggregate(ttl=Sum('sumDonations')).get('ttl', 0)
     return dict(
         sumDonations=float(tsum + psum),
