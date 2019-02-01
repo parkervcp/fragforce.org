fe75e79a8690053ce5e3df2f65912466d449635f
Paulson McIntyre <pmcintyre@salesforce.com>
2019-02-01 09:27:13 -0500

    Registering all models for admin interface

--- ffdonations/admin.py
+++ ffdonations/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(EventModel)
+admin.site.register(TeamModel)
+admin.site.register(ParticipantModel)
+admin.site.register(DonationModel)
+admin.site.register(MediaTiltifyModel)
+admin.site.register(RewardTiltifyModel)
+admin.site.register(SocailTiltifyModel)
+admin.site.register(AddressTiltifyModel)
+admin.site.register(ColorTiltifyModel)
+admin.site.register(SettingsTiltifyModel)
+admin.site.register(CauseTiltifyModel)
+admin.site.register(EventTiltifyModel)
+admin.site.register(LiveStreamTiltifyModel)
+admin.site.register(TeamTiltifyModel)
+admin.site.register(UserTiltifyModel)
+admin.site.register(CampaignTiltifyModel)
+admin.site.register(DonationTiltifyModel)
--- ffsfdc/admin.py
+++ ffsfdc/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Hcmeta)
+admin.site.register(SfEventLog)
+admin.site.register(TriggerLog)
+admin.site.register(TriggerLogArchive)
+admin.site.register(SiteAccount)
+admin.site.register(Contact)
+admin.site.register(ELHistory)
+admin.site.register(Event)
+admin.site.register(EventParticipant)
