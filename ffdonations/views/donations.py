656161dda28dc4ae9156da1ceb6ea09c0f43190d
Ben Gray <30706489+BenG-SFDC@users.noreply.github.com>
2019-02-12 22:10:27 -0800

    django get fix

--- ffdonations/views/donations.py
+++ ffdonations/views/donations.py
@@ -8,9 +8,7 @@ from django.conf import settings
 
 @cache_page(settings.VIEW_DONATIONS_CACHE)
 def v_donations(request):
-    orderByVar = request.GET['orderBy']
-    if orderByVar is None:
-        orderByVar = 'id'
+    orderByVar = request.GET.get('orderBy', 'id')
     update_donations_if_needed.delay()
     return JsonResponse(
         [d for d in DonationModel.objects.all().order_by(orderByVar).values()],
@@ -20,9 +18,7 @@ def v_donations(request):
 
 @cache_page(settings.VIEW_DONATIONS_CACHE)
 def v_tracked_donations(request):
-    orderByVar = request.GET['orderBy']
-    if orderByVar is None:
-        orderByVar = 'id'
+orderByVar = request.GET.get('orderBy', 'id')
     update_donations_if_needed.delay()
     return JsonResponse(
         [d for d in DonationModel.objects.filter(DonationModel.tracked_q()).order_by(orderByVar).values()],
