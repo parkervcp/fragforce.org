b152616cc0123d76db840aa9332bb7019a3fccbc
Paulson McIntyre <paul@gpmidi.net>
2019-03-24 11:30:21 -0400

    Adding donation chart

--- ffdonations/ctx.py
+++ ffdonations/ctx.py
@@ -6,8 +6,15 @@ from .utils import *
 
 def donations(request):
     """ Context processors for all ffdonations pages """
-    return dict(
+    ret = dict(
         # FIXME: Add custom caching here - use tasks to pregenerate so it doesn't slow down random pages
         el_donation_stats=el_donation_stats(),
         el_num_donations=el_num_donations(),
     )
+    ret['extralife'] = ret['el_donation_stats']['sumDonations']
+    ret['childsplay'] = 0  # Stubs for CP & Singapore
+    ret['singapore'] = 0  # Stubs for CP & Singapore
+    ret['sumDonations'] = ret['extralife'] + ret['childsplay'] + ret['singapore']
+    ret['target'] = 200000
+    ret['togo'] = ret['target'] - ret['sumDonations']
+    return ret
