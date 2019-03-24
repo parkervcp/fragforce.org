32f4b34c9f19915bd31f7ab4148d2d50fb3c95f8
Paulson McIntyre <paul@gpmidi.net>
2019-03-24 11:42:50 -0400

    Making floats

--- ffdonations/ctx.py
+++ ffdonations/ctx.py
@@ -12,8 +12,8 @@ def donations(request):
         el_num_donations=el_num_donations(),
     )
     ret['extralife'] = ret['el_donation_stats']['sumDonations']
-    ret['childsplay'] = 0  # Stubs for CP & Singapore
-    ret['singapore'] = 0  # Stubs for CP & Singapore
+    ret['childsplay'] = 0.0  # Stubs for CP & Singapore
+    ret['singapore'] = 0.0  # Stubs for CP & Singapore
     ret['sumDonations'] = ret['extralife'] + ret['childsplay'] + ret['singapore']
     ret['target'] = 200000
     ret['togo'] = ret['target'] - ret['sumDonations']
