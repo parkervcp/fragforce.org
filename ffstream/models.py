b591d231ed4732d4b808e19614e7e0d9f63a89ea
Paulson McIntyre <paul@gpmidi.net>
2020-05-09 20:36:02 -0400

    Adding admin back and stream tracking

---
+++ ffstream/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import uuid
+
+
+class Key(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    name = models.SlugField(max_length=256, unique=True)
+    owner = models.ForeignKey('ffsfdc.models.Contact', on_delete=models.CASCADE, null=False, blank=False)
+    created = models.DateTimeField(verbose_name="Created At", null=True)
+    modified = models.DateTimeField(null=False, auto_now=True, verbose_name="Modified At")
+    is_live = models.BooleanField(null=False, default=False, verbose_name="Is Live")
+    active = models.BooleanField(default=True, verbose_name="Can be used for streaming")
+
+
+class Stream(models.Model):
+    guid = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    key = models.ForeignKey(Key, on_delete=models.CASCADE, null=False, blank=False)
+    owner = models.ForeignKey('ffsfdc.models.Contact', on_delete=models.CASCADE, null=False, blank=False)
+    created = models.DateTimeField(verbose_name="Created At", auto_now_add=True)
+    modified = models.DateTimeField(null=False, auto_now=True, verbose_name="Modified At")
+    is_live = models.BooleanField(null=False, default=False, verbose_name="Is Live")
+    started = models.DateTimeField(verbose_name="Started Streaming At", null=True)
+    ended = models.DateTimeField(verbose_name="Ended Streaming At", null=True)
---
+++ ffstream/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from django.views.decorators.http import require_POST
+from .models import *
+
+
+@require_POST
+def start(request):
+    skey = request.POST['name']
+    key = get_object_or_404(Key, key=skey)
+    if not key.active:
+        return HttpResponseForbidden("inactive key")
+    # if key.is_live:
+    # What to do if already live?
+
+    key.is_live = True
+    key.save()
+
+    stream = Stream(key=key, owner=key.owner, is_live=True, started=timezone.now(), ended=None)
+    stream.save()
+
+    # Change key to GUID
+    return HttpResponseRedirect(stream.guid)
+
+
+@require_POST
+def stop(request):
+    skey = request.POST['name']
+    key = get_object_or_404(Key, key=skey)
+    key.is_live = False
+    key.save()
+    # End them all, just in case
+    for stream in key.stream_set.filter(is_live=True, ended=None).all():
+        stream.ended = timezone.now()
+        stream.is_live = False
+        stream.save()
+
+    return HttpResponse("OK")
