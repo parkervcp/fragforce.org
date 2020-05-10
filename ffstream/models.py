5bad25d80f2865ab48317f6dcfc970e16a2badcf
Paulson McIntyre <paul@gpmidi.net>
2020-05-09 20:37:55 -0400

    Fixed migration and made migration

--- ffstream/models.py
+++ ffstream/models.py
@@ -5,7 +5,7 @@ import uuid
 class Key(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     name = models.SlugField(max_length=256, unique=True)
-    owner = models.ForeignKey('ffsfdc.models.Contact', on_delete=models.CASCADE, null=False, blank=False)
+    owner = models.ForeignKey('ffsfdc.Contact', on_delete=models.CASCADE, null=False, blank=False)
     created = models.DateTimeField(verbose_name="Created At", null=True)
     modified = models.DateTimeField(null=False, auto_now=True, verbose_name="Modified At")
     is_live = models.BooleanField(null=False, default=False, verbose_name="Is Live")
@@ -15,7 +15,7 @@ class Key(models.Model):
 class Stream(models.Model):
     guid = models.UUIDField(default=uuid.uuid4, primary_key=True)
     key = models.ForeignKey(Key, on_delete=models.CASCADE, null=False, blank=False)
-    owner = models.ForeignKey('ffsfdc.models.Contact', on_delete=models.CASCADE, null=False, blank=False)
+    owner = models.ForeignKey('ffsfdc.Contact', on_delete=models.CASCADE, null=False, blank=False)
     created = models.DateTimeField(verbose_name="Created At", auto_now_add=True)
     modified = models.DateTimeField(null=False, auto_now=True, verbose_name="Modified At")
     is_live = models.BooleanField(null=False, default=False, verbose_name="Is Live")
