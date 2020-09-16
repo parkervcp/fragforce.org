06a9b7af63cdfea70308a4ac4be42e2677bc9e92
Paulson McIntyre <paul@gpmidi.net>
2020-09-16 14:22:46 -0400

    Formatting

--- ffstream/models.py
+++ ffstream/models.py
@@ -16,6 +16,7 @@ class Key(models.Model):
     def __str__(self):
         return self.name
 
+
 class Stream(models.Model):
     guid = models.UUIDField(default=uuid.uuid4, primary_key=True)
     key = models.ForeignKey(Key, on_delete=models.CASCADE, null=False, blank=False)
