0248490dbc40ed1e9fb3f29044bd48c929b7eb2f
Paulson McIntyre <paul@gpmidi.net>
2020-09-17 08:15:14 -0400

    Import cleanup

--- ffstream/views.py
+++ ffstream/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
 from .models import *
 
 
