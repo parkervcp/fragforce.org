35f2c71651976c4e0b8c69d00cfa4de9266406d5
Paulson McIntyre <paul@gpmidi.net>
2020-09-26 18:26:47 -0400

    Move test

--- ffdonations/views/stats.py
+++ ffdonations/views/stats.py
@@ -3,10 +3,10 @@ from django.conf import settings
 from django.db.models import Avg, Max, Min, Sum
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
-from ..ctx import donations
 from ..tasks import *
 
 
 @cache_page(settings.VIEW_DONATIONS_STATS_CACHE)
 def v_tracked_donations_stats(request):
+    from ..ctx import donations
     return JsonResponse(donations(request=request))
